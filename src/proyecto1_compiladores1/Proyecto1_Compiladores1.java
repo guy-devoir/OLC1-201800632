f5172acb2c848eb4b39175af29a242904a66d62b
Guy-devoir <78318071+Guy-devoir@users.noreply.github.com>
2022-09-07 00:19:04 -0600

    Initial commit

---
+++ src/proyecto1_compiladores1/Proyecto1_Compiladores1.java
@@ -0,0 +1,13 @@
+package proyecto1_compiladores1;
+
+/**
+ * @author Luciano Xiquín
+ */
+public class Proyecto1_Compiladores1 {
+
+    public static void main(String[] args) {
+        Interface window = new Interface();
+        window.setVisible(true);
+    }
+    
+}
