0e95ee9ad80f29db3cbf27a6a46adc1b72883a94
Guy-devoir <78318071+Guy-devoir@users.noreply.github.com>
2022-09-17 23:48:26 -0600

    final

--- src/proyecto1_compiladores1/Proyecto1_Compiladores1.java
+++ src/proyecto1_compiladores1/Proyecto1_Compiladores1.java
@@ -15,10 +15,6 @@ public class Proyecto1_Compiladores1 {
     public static void main(String[] args) throws Exception {
         Interface window = new Interface();
         window.setVisible(true);
-        String one =  "C:\\Users\\Luciano Xiquín\\Documents\\NetBeansProjects\\Proyecto1_Compiladores1\\src\\analizadores\\Lexico";
-        String two = "C:\\Users\\Luciano Xiquín\\Documents\\NetBeansProjects\\Proyecto1_Compiladores1\\src\\analizadores\\Sintactico";
-        String[] rutaS = {"-parser", "Sintax", "C:\\Users\\Luciano Xiquín\\Documents\\NetBeansProjects\\Proyecto1_Compiladores1\\src\\analizadores\\Sintactico.cup"};
-        generar(one,two, rutaS);
     }
     
      public static void generar(String ruta1, String ruta2, String[] rutaS) throws IOException, Exception{
